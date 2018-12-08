cb0dba0d9afdc785d054bc71902e1b6ac78ba5ef
Agnieszka Polichnowska <apolichnowska@tlen.pl>
2018-12-08 15:00:44 +0100

    Add files

--- js/script.js
+++ js/script.js
@@ -1,34 +1,31 @@
 'use strict';
-(function () {
-    var url = 'https://restcountries.eu/rest/v2/name/';
-    var countriesList = $('#countries');
+(function() {
+	var url = 'https://restcountries.eu/rest/v2/name/';
+	var countriesList = $('#countries');
 	/*Na kliknięcie w "szukaj" należy uruchomić pewną funkcję. Nazwijmy ją searchCountries().  */
 	$('#search').click(searchCountries); /*podpięcie żądania pod przycisk z id search */
 	/*Zanim wywołamy samo wyszukiwanie, musimy pobrać wartość wpisaną przez 
 	użytkownika i przypisać do zmiennej- metoda val(): */
 	function searchCountries() {
-        var countryName = $('#country-name').val();
+		var countryName = $('#country-name').val();
 		/* warunek sprawdzający, czy pole tekstowe nie jest 
 		przypadkiem puste. Jeśli tak, ustawmy jego wartość na "Poland". */
 		if (!countryName.length) countryName = 'Poland';
-        $.ajax({
-            url: url + countryName,
-            method: 'GET',
-            success: showCountriesList
-        });
-    }
+		$.ajax({
+			url: url + countryName,
+			method: 'GET',
+			success: showCountriesList
+		});
+	}
 	/*w kodzie użyliśmy parametru, który określa, jaka funkcja ma zostać wykonana
 w przypadku powodzenia zapytania - jest to showCountriesList() */
 	/*Pobieranie wyników 
 	1. wyczyszczenie listy krajów po poprzednim zapytaniu. Do tego celu na liście użyjemy metody empty().
 	2. */
 	function showCountriesList(resp) {
-        countriesList.empty();
-        resp.forEach(function (item) {
-            $('<li>').text(item.name).appendTo(countriesList);
-            if (item.capital) {
-                $('<span>').text(', ' + item.capital).appendTo($('#countries > li:last'));
-            }
-        });
-    }
+		countriesList.empty();
+		resp.forEach(function(item) {
+			$('<li>').text(item.name).appendTo(countriesList);
+		});
+	}
 })();
